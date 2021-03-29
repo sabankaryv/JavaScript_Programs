3ceb2a762c3c691d469f31ac4a6ea7a5e08b6db7
sabankaryv <sabankaryv@gmail.com>
2021-03-30 01:43:06 +0530

    Third Commit

---
+++ operators.js
@@ -0,0 +1,34 @@
+var name="Yogesh";
+var surname="Sabankar";
+
+
+console.log(name+surname);
+console.log("Solapur"+"Pune");
+
+
+
+var num1=10;
+
+var num2=20;
+
+
+console.log(num1+num2);
+
+
+
+var age=19;
+
+
+if(age>=18){
+    console.log("You are eligible for vote");
+}
+else{
+    console.log("You are not elibible for vote");
+}
+
+
+
+var age=18;
+
+
+console.log((age>=18)?"You Can vote Bro":"You can't vote man");
