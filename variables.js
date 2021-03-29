ce49a7fd66d3bf58285c9fa6ab421cafcd7cb62f
sabankaryv <sabankaryv@gmail.com>
2021-03-30 01:38:06 +0530

    first commit

---
+++ variables.js
@@ -0,0 +1,26 @@
+var myName="Yogesh Sabankar";
+
+console.log(myName);
+
+//var name="Yogii";
+
+//console.log(1name);
+//(RSSN)
+//1.variable name can not start with number
+//2.variable name can not use special characters
+//3.variable name can not start space
+//4.Most Important we can not use reserved keyword for variale like var.
+
+
+var myName1="Yogesh Sabankar";
+console.log(myName1);
+
+//var cityName%="Pune";
+
+//console.log(cityName!);
+
+var __name="Pune";
+console.log(__name);
+
+var $state="Maharashtra";
+console.log($state);
